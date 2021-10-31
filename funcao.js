0dda79891a98ee748d9d83596ed63c641e7e9208
anacarolinesilva <anasilva.caroline@hotmail.com>
2021-10-30 22:58:30 -0300

    commit

---
+++ funcao.js
@@ -0,0 +1,11 @@
+// Verificar se o número é par ou impar
+
+function parimpar(n) {
+    if (n % 2 == 0) {
+        return 'Par'
+    } else {
+        return 'Impar'
+    }
+}
+
+console.log(parimpar(101))
